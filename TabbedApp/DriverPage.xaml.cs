3737d372f5f3b8a7fc9f946eff1df5cb70ae1d86
Lucas <lwolfs@my.wctc.edu>
2020-04-13 22:19:39 -0500

    Add project files.

---
+++ TabbedApp/DriverPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DriverPage : ContentPage
+    {
+        public DriverPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            int age = Convert.ToInt16(ageEntry.Text);
+            if (age >= 16)
+            {
+                outputLabel.Text = "You have been able to drive for up to " + (age - 16).ToString() + " years.";
+
+            }
+            else
+            {
+                outputLabel.Text = "You cant drive yet.";
+            }
+
+        }
+    }
+}
